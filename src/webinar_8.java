6635afa8f1ce5b47b888bd3d81253a3b27ee240a
lukaszzajaczkowski <clancys49@gmail.com>
2021-09-25 18:03:14 +0200

    homework_8

---
+++ src/webinar_8.java
@@ -0,0 +1,9 @@
+public class webinar_8 {
+    public static void main(String[] args) {
+        //Zmodyfikuj, aby po klasie Person dziedziczyła klasa pracownik-uczelnia a dopiero po pracownik-uczelni niech dziedziczy:
+        // profesor, pracownik-administracji
+        //Zaproponuj pola oraz metody wspólne oraz (nie wspólne) czyli w konkretnych klasach
+        //7:16
+        //Zaprezentuj działanie w klasie Main
+    }
+}
